75aa5c0a33a29fcfd9c2bdf195bb49773f00a7ea
isaac <baa44422@gmail.com>
2021-07-30 19:01:17 +0800

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Isaac Lee Jing Zhi on 30/7/21.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
